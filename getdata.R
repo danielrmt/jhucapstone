852f2dff9d95cd242609b7ddb89521eeed493f47
Daniel Ribeiro <danielrmt@gmail.com>
2019-08-17 11:50:55 -0300

    Subset the data with training and testing samples

--- getdata.R
+++ getdata.R
@@ -9,3 +9,22 @@ if (!file.exists('data/Coursera-SwiftKey.zip')) {
   )
   unzip('data/Coursera-SwiftKey.zip', exdir='data')
 }
+
+
+
+dir.create('data/training', recursive = TRUE)
+dir.create('data/testing', recursive = TRUE)
+set.seed(12345)
+sample_size <- .20
+for (fn in Sys.glob('data/final/en_US/*.txt')) {
+  data <- readLines(fn)
+  
+  training_sample <- sample(data, length(data) * sample_size)
+  writeLines(training_sample, file.path('data', 'training', basename(fn)))
+  
+  data <- data[rownames(data)!=rownames(training_sample)]
+  testing_sample <- sample(data, length(data) * sample_size)
+  writeLines(testing_sample, file.path('data', 'testing', basename(fn)))
+  gc()
+  rm(data, training_sample, testing_sample)
+}
